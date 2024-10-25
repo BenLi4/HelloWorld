4012af05d278cbc7ec8843547063b2f025a1a9bd
BT_1N3_18 <sample@bths.edu>
2024-10-25 13:09:51 -0400

    gjjhjkldghsjklhsjkl;

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,16 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("fgqwuoghzsg");
-        int x = 56;
-        System.out.println(x);
-        System.out.println("erjklghashguorhiohgiohbkehiog");
+        int y = 56;
+        System.out.println(y);
+        System.out.println("erjklghashuorhiohgiohbkehiog");
+        int k = 5890672;
+        System.out.println(y + k);
+        System.out.println("ahgjahgjah");
+        if (k > y) {
+            System.out.println("GEHAHAHAHAHAHAHAHAHAHA");
+        }
+
+        System.out.println("sguafhijhhh");
     }
 }
